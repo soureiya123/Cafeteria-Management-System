d93b6e31a07c1e6a81e25c81bae9e4962301afb0
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-04-05 18:37:11 +0100

    some more work on the auth #login not yet working

--- auth.h
+++ auth.h
@@ -12,23 +12,25 @@ void takeInput();
 #define TAB 9
 #define BKSPC 8
 
-struct user{
+struct User{
 
         char id[50];
         char course[50];
         char password[50];
-        char role[50]
-    } user;
+        char role[50];
+    };
+
+    struct User user;
 
 int choice;
-int finalPasswd;
+char finalPasswd[50];
 FILE *fp;
 char studentnId[50];
 char studentPass[50];
-struct user student;
+struct User student;
 
 
-void takPassword(char passd[50]) {
+void takePassword(char passd[50]) {
 
          int i;
          char ch;
@@ -57,51 +59,55 @@ while(1){
 
 void takInput(char input[50]){
     fgets(input,50,stdin);//fgets stands for file get string. It is included in the C standard library header file stdio
-    fgets(input,50,stdin);//fgets stands for file get string. It is included in the C standard library header file stdio
     input[strlen(input)-1]=0;
 }
 
 int login(){
-   printf("Enter your ID");
-    takInput(studentnId);
-    printf("Enter your password");
-    takInput(studentPass);
-    fp=fopen(" Students.data","r");
-    while(fread(&student,sizeof(struct user),1,fp)){
-            if(!strcmp(student.id,studentnId)){
-                if(!strcmp(student.password,studentPass)){
+   printf("Enter your ID: ");
+    scanf("%s",studentnId);
+    printf("Enter your password: ");
+    scanf("%s",studentPass);
+    FILE *fp=fopen(" Students.data","r");
+    while(fread(&student,sizeof(struct User),1,fp)){
+        printf("%s",student.id);
+            if(strcmp(student.id,studentnId)){
+                if(strcmp(student.password,studentPass)){
 
                         printf("welcome");
+                        return 1;
                 }else{
 
                     printf("Invalid password");
                     Beep(800,300);
+                    return 0;
                 }
             }
     }
 }
 
 int signup(){
-    printf("Enter your id: \t");
-    takInput(user.id);
-    printf("Enter your course: \t");
-    takInput(user.course);
-    printf("Enter your password: \t");
-    takPassword(user.password);
-    printf("Confirm your password: \t");
-    takPassword(finalPasswd);
-    printf("Enter your password: \t");
-    scanf("%c",&finalPasswd);
-    if (!strcmp(user.password,finalPasswd))
+    struct User *userptr = malloc(sizeof *userptr);
+    printf("Enter your id: ");
+    scanf("%s",userptr->id);
+    printf("Enter your course: ");
+    scanf("%s",userptr->course);
+    printf("Enter your password: ");
+    scanf("%s",userptr->password);
+    printf("Confirm your password: ");
+    scanf("%s",finalPasswd);
+    if (strcmp(user.password,finalPasswd))
     {
         printf("your password matched");
         fp=fopen(" Students.data","a+");
-        fwrite(&user,sizeof(struct user),1,fp);
+        fwrite(userptr,sizeof(struct User),1,fp);
 
-        if(fwrite!=0)
+        if(fwrite!=0){
             printf(" Students registration is succed");
-        else
-        printf(" Sorry,so,ething went wrong");
+        }
+        else{
+            printf(" Sorry,so,ething went wrong");
+        }
+        fclose(fp);
     }
     else{
 
--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 //define includes
 #include "auth.h"
 
+//home menu prints
+void homemenu(){
+    printf("Select from the menu below what you will want to do /n\n You can always press 0 to exit \n\n");
+    printf("1: View todays menu \n");
+    printf("2: New choice \n");
+}
+
 int login();
 
 int main(){
@@ -21,7 +28,12 @@ int main(){
             signup();
             break;
         case 2:
-            login();
+            if(login()){
+                int selected;
+                while(scanf("%d",&selected) != 0){
+                    homemenu();
+                }
+            }
             break;
     }
 }
