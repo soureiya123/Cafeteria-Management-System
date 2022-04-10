50e556155cf829d92f788f0e55360d3e3f269f35
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-04-10 10:26:51 +0100

    more work done

--- main.c
+++ main.c
@@ -6,16 +6,82 @@
 
 //home menu prints
 void homemenu(){
-    printf("Select from the menu below what you will want to do /n\n You can always press 0 to exit \n\n");
+    int selection;
+    int day_selected;
+    system("cls");
+    printf("------------------WELCOME-------------------------- \n \n \n");
+    printf("Select from the menu below what you will want to do \n You can always press 0 to exit \n");
     printf("1: View todays menu \n");
     printf("2: New choice \n");
+    
+    do {
+        printf("Enter your choice: ");
+        scanf("%d",&selection);
+
+        if(selection == 1){
+            printf("Todays menu here");
+            break;
+        }else if(selection == 2){
+            printf("\n\nSelect a day from below \n");
+            printf("1 \t Monday \n");
+            printf("2 \t Tuesday \n");
+            printf("3 \t Wednesday \n");
+            printf("4 \t Thursday \n");
+            printf("5 \t Friday \n");
+            printf("6 \t Saturday \n");
+            printf("7 \t Sunday \n");
+
+            printf("Enter selected day: ");
+            scanf("%d",&day_selected);
+
+            switch (day_selected)
+            {
+            case 1:
+                printf("you have selected monday");
+                break;
+            case 2:
+                printf("you have selected Tuesday");
+                break;
+            case 3:
+                printf("you have selected Wednesday");
+                break;
+            case 4:
+                printf("you have selected Thursday");
+                break;
+            case 5:
+                printf("you have selected Friday");
+                break;
+            case 6:
+                printf("you have selected Saturday");
+                break;
+            case 7:
+                printf("you have selected Sunday");
+                break;
+            default:
+                printf("Invalid day selected");
+                break;
+            }
+
+            break;
+        }
+    }while( selection != 0 || selection > 2);
+}
+
+void login_user(){
+    int loginr = 0;
+    loginr = login();
+    if(loginr == 1){
+        int selected;
+        homemenu();
+    }else{
+        login_user();
+    }
 }
 
 int login();
 
 int main(){
     int choice;
-    int loginr = 0;
 
     printf("1:sign up\n");
     printf("2 login\n");
@@ -29,15 +95,7 @@ int main(){
             signup();
             break;
         case 2:
-            
-            loginr = login();
-            printf("%d \n",loginr);
-            if(loginr == 1){
-                int selected;
-                homemenu();
-            }else{
-                main();
-            }
+            login_user();
             break;
         default:
             printf("Exiting ........");
